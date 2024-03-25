e9cf08f60c6030a34f386996c9ec8dcffec4ec14
allexkap <aekpstn@gmail.com>
2024-03-25 11:28:11 +0300

    move db class to another file

--- observer.py
+++ observer.py
@@ -1,6 +1,5 @@
 import json
 import re
-import sqlite3
 import sys
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
@@ -9,45 +8,7 @@ from time import sleep
 
 import requests
 
-
-class Activity:
-
-    _create_table_cmd = '''
-        create table if not exists activity (
-            timestamp integer not null,
-            account text not null,
-            hours real
-        ) strict
-    '''
-    _insert_row_cmd = 'insert into activity values (?, ?, ?)'
-    _select_tail_cmd = 'select * from activity order by timestamp desc limit ?'
-    _select_last_by_user_cmd = '''
-        select * from activity
-        where account = ? and hours != 0
-        order by timestamp desc limit 1
-    '''
-
-    def __init__(self, path: Path):
-        self.con = sqlite3.connect(path)
-        self.cur = self.con.cursor()
-        self.cur.execute(self._create_table_cmd)
-        self.con.commit()
-
-    def insert(self, user: str, value: float):
-        ts = int(datetime.now().timestamp())
-        self.cur.execute(self._insert_row_cmd, (ts, user, value))
-        self.con.commit()
-
-    def get_last(self, user: str):
-        res = self.cur.execute(self._select_last_by_user_cmd, (user,))
-        return res.fetchone()
-
-    def tail(self, n: int = 5):
-        res = self.cur.execute(self._select_tail_cmd, (n,))
-        return res.fetchall()
-
-    def __del__(self):
-        self.con.close()
+from db import Activity
 
 
 def assert_activity(activity: Activity, username: str, hours: float):
--- select_last.py
+++ select_last.py
@@ -1,8 +1,9 @@
 from datetime import datetime as dt
-import sqlite3
+from pathlib import Path
 
-con = sqlite3.connect('db.sqlite3')
-cur = con.cursor()
-d = cur.execute('select * from activity order by timestamp desc limit 9').fetchall()
+from db import Activity
+
+activity = Activity(Path('db.sqlite3'))
+d = activity.tail(9)
 r = '\n'.join(f'{dt.fromtimestamp(e[0]).ctime()}; {e[1]:>9}; {e[2]:5}' for e in d)
 print(r)
